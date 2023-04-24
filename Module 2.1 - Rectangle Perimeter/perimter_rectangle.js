3f96eedb72da214ac02a3bfd397147f2410cf154
19miguel19lim <117153385+19miguel19lim@users.noreply.github.com>
2023-04-24 17:06:46 +0800

    Module 2.1

---
+++ Module 2.1 - Rectangle Perimeter/perimter_rectangle.js
@@ -0,0 +1,11 @@
+const width = 16;
+const length = 20;
+
+const perimeter = function (width, length) {
+  p = 2 * width + 2 * length;
+  console.log(`Width: ${width}`);
+  console.log(`length: ${length}`);
+  console.log(`Perimeter: ${p}`);
+};
+
+perimeter(width, length);
