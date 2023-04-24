0f01a36321db2daa6ec748785ac47100ebcad019
19miguel19lim <117153385+19miguel19lim@users.noreply.github.com>
2023-04-24 17:06:05 +0800

    Module 2.1

---
+++ Module 2.1 - Rectangle Area/area_rectangle.js
@@ -0,0 +1,11 @@
+const width = 16;
+const length = 20;
+
+const perimeter = function (width, length) {
+  a = width * length;
+  console.log(`Width: ${width}`);
+  console.log(`length: ${length}`);
+  console.log(`Area: ${a}`);
+};
+
+perimeter(width, length);
